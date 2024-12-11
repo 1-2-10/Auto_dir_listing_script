73ac39e10f1c2b3d64abb405fa2a62069ecb1e99
1-2-10 <48568372+1-2-10@users.noreply.github.com>
2024-12-11 12:03:29 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>brdcrmbs_test</title>
+    <!-- Add any CSS or stylesheets here -->
+</head>
+<body>
+    <h1>brdcrmbs_test</h1>
+    <?php include "../brdcrmbs_hm_plate.php"; ?>
+     
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consequat ultricies ligula, id laoreet nunc condimentum eget.</p>
+    <p>Nullam ultrices ultricies mi, at tincidunt nisi. Morbi sed tellus non quam suscipit facilisis vel a turpis.</p>
+</body>
+</html>
+
